6128c5eb726f0770358963a940fea2e15a305e48
Aries Abao <[ariesabao@live.com](mailto:ariesabao@live.com)>
2018-09-03 22:58:02 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Isbn></Isbn>
+  </div>
+</template>
+
+<script>
+import Isbn from './components/Isbn'
+
+export default {
+  name: 'App',
+  components: {
+    Isbn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Isbn.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="isbn">
+    <input v-model="msg">
+    <br />
+    <h1>{{ checkDigit() }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Isbn',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    checkDigit: function() {
+     var sum,
+        weight,
+        digit,
+        check,
+        i;
+
+    this.msg = this.msg.replace(/[^0-9X]/gi, '');
+
+    if (this.msg.length != 10) {
+        //return false;
+    }
+
+    if (this.msg.length == 10) {
+        weight = 10;
+        sum = 0;
+        for (i = 0; i < 9; i++) {
+            digit = parseInt(this.msg[i]);
+            sum += weight*digit;
+            weight--;
+        }
+        check = 11 - (sum % 11);
+        if (check == 10) {
+            check = 'X';
+        }
+        if (check == this.msg[this.msg.length-1].toUpperCase() == true){
+          return this.msg + ' is Valid';
+        } else {
+          return this.msg + ' is Not Valid';
+        }
+    }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
